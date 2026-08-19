e77dd5c708829565a55f3f420eddd498c1766271
karimmejbar2003-alt <karimmejbar2003@gmail.com>
2026-08-20 00:33:23 +0200

    Bump SW cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caissepharma-v2';
+const CACHE = 'caissepharma-v3';
 const ASSETS = ['.', 'index.html', 'style.css', 'app.js', 'manifest.json', 'icon.svg', 'firebase-config.js'];
 
 self.addEventListener('install', e =>
